b899d8595ccec7a09e4b9ee8356cf59d5008935a
NailimN <milian.normantlegall@gmail.com>
2025-10-08 13:18:13 +0200

    Ajout class Filiere

---
+++ src/main/java/demo/model/Filiere.java
@@ -0,0 +1,21 @@
+package demo.model;
+
+public class Filiere {
+	
+
+	String libele;
+	
+	public Filiere(String libele) {
+		super();
+		this.libele = libele;
+	}
+
+	public String getLibele() {
+		return libele;
+	}
+
+	public void setLibele(String libele) {
+		this.libele = libele;
+	}
+	
+}
