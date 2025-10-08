88338379a2f27ffbde0f1ac601fa1437823b6539
NailimN <milian.normantlegall@gmail.com>
2025-10-08 12:37:38 +0200

    first commit

---
+++ src/main/java/demo/dao/IDAO.java
@@ -0,0 +1,18 @@
+package demo.dao;
+
+import java.util.List;
+
+public interface IDAO<T,K> {
+
+
+	String urlBdd = "jdbc:mysql://localhost:3306/hopital";
+	String loginBdd = "root";
+	String passwordBdd = "root";
+	
+	public List<T> findAll();
+	public T findById(K id);
+	public void insert(T obj);
+	public void update(T obj);
+	public void delete(K id);
+}
+
